a087d17044c6f3c182d83a68df7ba7779912df5e
Saurabh <sbansod@iitk.ac.in>
2015-03-01 08:09:00 +0530

    finished the sample app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ActionController::Base
+
+  def index
+  	# Used when token is required
+    # @twitch = Twitch.new({
+    #   :client_id => ENV['client_id'],
+    #   :secret_key => ENV['secret_key'],
+    #   :redirect_uri => ENV['redirect_uri'],
+    #   :scope => ["user_read", "channel_read", "user_follows_edit"]
+    # })
+    @twitch = Twitch.new()
+    # @data = @twitch.getTopGames()
+    @data = @twitch.getTopGames()[:body]['top']
+  end
+end
